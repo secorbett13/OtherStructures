1967f9cffce5642cd074953892241a92594a3664
Scott Corbett <secorbett13@gmail.com>
2014-02-20 08:10:01 -0800

    First unit test for the Rope constructor

---
+++ OtherStructures/Arrays/GapBuffer.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GapBuffer.cs" company="Scott Corbett">
+//   Copyright 2014, Scott Corbett. All rights reserved. More than likely the rights are going to be opened up. For now,
+//   all users are entitled to use the code as long as they include this notice in the header of any files used directly.
+//   Any compiled library made for Other Structures can be used free of charge. If the library is decompiled, the
+//   copyright notice needs to be inserted into the header of the decompiled files.
+// </copyright>
+// <summary>
+//   Defines the GapBuffer type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OtherStructures
+{
+	/// <summary>
+	/// Implements the Gap Buffer data structure in .Net. The Gap Buffer structure allow for the efficient insertion and
+	/// deletion operations clustered near the same location in an array.
+	/// </summary>
+	/// <remarks>
+	/// See http://en.wikipedia.org/wiki/Gap_buffer for more information.
+	/// </remarks>
+	public class GapBuffer
+	{
+		 
+	}
+}
